be0a3772993edd57c7db2d374db5fb0d32e9e647
Abu Ghalib <abughalib64@gmail.com>
2021-05-20 01:42:34 +0530

    added sign up

--- app/src/main/java/com/dev/bcepedia/bceplacecom/ui/auth/SignUpActivity.kt
+++ app/src/main/java/com/dev/bcepedia/bceplacecom/ui/auth/SignUpActivity.kt
@@ -1,21 +1,41 @@
 package com.dev.bcepedia.bceplacecom.ui.auth
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.dev.bcepedia.bceplacecom.R
+import com.dev.bcepedia.bceplacecom.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignUpActivity : AppCompatActivity() {
 
+  private lateinit var binding: ActivitySignUpBinding
   private lateinit var auth: FirebaseAuth
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_sign_up)
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+    auth = Firebase.auth
 
+    // Sign In Button
+    binding.signInButton.setOnClickListener {
+      val intent: Intent = Intent(this, LoginActivity::class.java)
+      startActivity(intent)
+    }
+
+    // Sign Up Button
+    binding.signUpButton.setOnClickListener{
+      val email: String = binding.signUpEmail.text.toString()
+      val pass: String = binding.signUpPass.text.toString()
+      createUserWithEmailAndPassword(email, pass)
+    }
+
+
+    binding.root
   }
 
   private fun createUserWithEmailAndPassword(email: String, password: String){
@@ -32,4 +52,5 @@ class SignUpActivity : AppCompatActivity() {
         }
       }
   }
+
 }
