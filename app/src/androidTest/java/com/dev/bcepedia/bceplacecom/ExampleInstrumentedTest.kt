591b71f11e29cebcf29bcb022e0f66d990c04ade
Abu Ghalib <abughalib64@gmail.com>
2021-05-20 02:05:14 +0530

    MainActivityLaunch test

--- app/src/androidTest/java/com/dev/bcepedia/bceplacecom/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/dev/bcepedia/bceplacecom/ExampleInstrumentedTest.kt
@@ -1,12 +1,17 @@
 package com.dev.bcepedia.bceplacecom
 
+import android.app.Activity
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +25,13 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.dev.bcepedia.bceplacecom", appContext.packageName)
+    }
+    @RunWith(AndroidJUnit4::class)
+    @LargeTest
+    class MainActivityTest{
+        @Test fun mainActivityLaunch(){
+            val scenario = ActivityScenario.launch(MainActivity::class.java)
+            assertEquals(scenario.result.resultCode, Activity.RESULT_OK)
+        }
     }
 }
